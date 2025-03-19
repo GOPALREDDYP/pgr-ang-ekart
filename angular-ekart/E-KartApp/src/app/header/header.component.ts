fa7540e50605550878bbace9409bd3b376400076
gopal reddy p <gopalreddy.pothula@gmail.com>
2025-03-20 01:07:03 +0530

    component
    Header component creation

---
+++ angular-ekart/E-KartApp/src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    template : '<h3> E-Kart Header</h3>'
+    
+
+})
+
+
+export class HeaderComponent{
+
+
+}
