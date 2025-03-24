68202c638119346f9032f0fbaaddd41288b7ed46
gopal reddy p <gopalreddy.pothula@gmail.com>
2025-03-25 01:58:26 +0530

    Components
    component changes

--- angular-ekart/E-KartApp/src/app/header/header.component.ts
+++ angular-ekart/E-KartApp/src/app/header/header.component.ts
@@ -1,14 +1,10 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-header',
-    template : '<h3> E-Kart Header</h3>'
-    
-
+    selector:'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
-
-
 export class HeaderComponent{
-
 
 }
